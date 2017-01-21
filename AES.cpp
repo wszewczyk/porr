7dc97a24bfa1391c1aea72215a87bc1a74701dae
Wlodzimierz Szewczyk <wlodzimierz.szewczyk@qualent.eu>
2017-01-21 16:05:36 +0100

    Refactored code.

--- AES.cpp
+++ AES.cpp
@@ -1,26 +1,7 @@
-
-#pragma once
 #include "stdafx.h"
-#include <omp.h>
-#include <stdio.h>
-#include <iostream>
-# include <string>
-#include <sstream>
-#include <vector>
-#include <mpi.h>
-#include<ctime>
+#include "AES.h"
 
-using namespace std;
-
-unsigned char Rcon[40] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36,
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-
-};
-
-
-unsigned char s[256] =
+unsigned char AES::sbox[] =
 {
 	0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
 	0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -40,346 +21,52 @@ unsigned char s[256] =
 	0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
 };
 
-
-struct HexCharStruct
+unsigned char AES::Rcon[] =
 {
-	unsigned char c;
-	HexCharStruct(unsigned char _c) : c(_c) { }
+	0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-inline std::ostream& operator<<(std::ostream& o, const HexCharStruct& hs)
+std::vector<std::vector<unsigned char>> AES::GenerateKey(std::string input)
 {
-	return (o << std::hex << (int)hs.c);
-}
-
-inline HexCharStruct hex(unsigned char _c)
-{
-	return HexCharStruct(_c);
-}
-
-
-
-struct AES_S
-{
-	unsigned char sbox[256];
-
-	std::vector<vector<unsigned char> > keys;
-
-	unsigned char** GetSubBytes(unsigned char**  tablica)
-	{
-		unsigned char **newchar = new unsigned char *[4];
-		for (int i = 0; i < 4; i++)
-		{
-			newchar[i] = new unsigned char[4];
-			for (int j = 0; j < 4; j++)
-			{
-				newchar[i][j] = getsboxvalue(tablica[i][j]);
-
-				//cout << hex(newchar[i][j]) << "  ";
-			}
-			//cout << endl;
-		}
-		return newchar;
-	}
-
-	unsigned char getsboxvalue(unsigned char x)
-	{
-		unsigned char d;
-		for (int i = 0; i < 256; i++)
-		{
-			if ((int)x == i)
-			{
-				d = sbox[i];
-
-				break;
-			}
-		}
-		return d;
-	}
-
-	unsigned char ** GetShiftRowTable(unsigned char ** firsttable)
-	{
-		unsigned char** mat = new unsigned char*[4];
-		int j = 0;
-		for (int i = 0; i < 4; i++)
-		{
-			mat[i] = new unsigned char[4];
-			unsigned char* dd = new unsigned char[4];
-			for (int d = 0; d < j; d++)
-			{
-				dd = RotateLeft(firsttable[i]);
-			}
-			j++;
-		}
-		return firsttable;
-	}
-
-	unsigned char* RotateLeft(unsigned char * row)
-	{
-		unsigned char* buffer = new unsigned char;
-		*buffer = row[0];
-		for (int i = 0; i < 3; i++)
-		{
-			row[i] = row[i + 1];
-		}
-		row[3] = *buffer;
-		return row;
-	}
-
-	unsigned char ** MixColumn(unsigned char ** matrix)
-	{
-		unsigned char **mat = new unsigned char*[4];
-		unsigned char column[4];
-		for (int i = 0; i < 4; i++)
-		{
-			mat[i] = new unsigned char[4];
-
-		}
-		for (int j = 0; j < 4; j++)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				column[i] = matrix[i][j];
-			}
-			gmix_column(column);
-			for (int i = 0; i < 4; i++)
-			{
-				mat[i][j] = column[i];
-			}
-		}
-		return mat;
-	}
-
-	unsigned char** AddRoundKey(unsigned char** matrix, unsigned char** key, int size = 4)
-	{
-		unsigned char**result = new unsigned char*[size];
-		for (int i = 0; i < size; i++)
-		{
-			result[i] = new unsigned char[size];
-			for (int j = 0; j < size; j++)
-			{
-				result[i][j] = matrix[i][j] ^ key[i][j];
-			}
-		}
-		return result;
-	}
-
-	void gmix_column(unsigned char r[]) {		// ta funkcja jest skopiowana z neta, srednio ja ogarniam
-		unsigned char a[4];	// operacja mixcolumns
-		unsigned char b[4];
-		unsigned char c;
-		unsigned char h;
-		/* The array 'a' is simply a copy of the input array 'r'
-		* The array 'b' is each element of the array 'a' multiplied by 2
-		* in Rijndael's Galois field
-		* a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field */
-		for (c = 0; c < 4; c++) {
-			a[c] = r[c];
-			/* h is 0xff if the high bit of r[c] is set, 0 otherwise */
-			h = (unsigned char)((signed char)r[c] >> 7);
-			/* arithmetic right shift, thus shifting in either zeros or ones */
-			b[c] = r[c] << 1;
-			/* implicitly removes high bit because b[c] is an 8-bit char,
-			so we xor by 0x1b and not 0x11b in the next line */
-			b[c] ^= 0x1B & h; /* Rijndael's Galois field */
-		}
-		r[0] = (b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]); /* 2 * a0 + a3 + a2 + 3 * a1 */
-		r[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; /* 2 * a1 + a0 + a3 + 3 * a2 */
-		r[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; /* 2 * a2 + a1 + a0 + 3 * a3 */
-		r[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
-	}
-
-
-	void InitializeArray(unsigned char** Array, int x = 4, int y = 4)
-	{
-		Array = new unsigned char*[x];
-		for (int i = 0; i < x; i++)
-		{
-			Array[i] = new unsigned char[y];
-
-		}
-	}
-
-
-	vector<vector<unsigned char> > RotateAndSubByteKeyColumn(vector<vector<unsigned char> > vect, int Columnindex)
-	{
-		unsigned char buffer[4];
-		for (int i = 0; i < vect.size(); i++)
-		{
-			buffer[i] = vect[i][Columnindex];
-
-		}
-
-		unsigned char a = buffer[0];
-		for (int i = 0; i < 3; i++)
-		{
-			buffer[i] = buffer[i + 1];
-		}
-		buffer[3] = a;
-		for (int i = 0; i < vect.size(); i++)
-		{
-			vect[i][Columnindex] = buffer[i];
-			vect[i][Columnindex] = getsboxvalue(vect[i][Columnindex]);
-
-		}
-
-		return vect;
-	}
-
-
-	vector<unsigned char> MixColumns(vector<vector<unsigned char> > vect1, int IterationNumber)
-	{
-		unsigned char first[4];
-		unsigned char second[4];
-		vector<unsigned char> result(4);
-		if (IterationNumber % 4 == 0)
-		{	//PrintVector(vect1);
-			vect1 = RotateAndSubByteKeyColumn(vect1, IterationNumber + 3);
-			//PrintVector(vect1);
-			for (int i = 0; i < 4; i++)
-			{
-				first[i] = vect1[i][IterationNumber];
-				second[i] = vect1[i][IterationNumber + 3];
-				result[i] = first[i] ^ second[i] ^ Rcon[10 * i + IterationNumber / 4];
-				//cout<<endl<< hex(first[i]) <<"  "<<hex(second[i])<<"  "<<hex(Rcon[10*i + IterationNumber])<<"  "<<hex(result[i])<<endl;
-			}
-		}
-		else
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				first[i] = vect1[i][IterationNumber];
-				second[i] = vect1[i][IterationNumber + 3];
-				result[i] = first[i] ^ second[i];
-			}
-		}
-
-		return result;
-	}
-
-	unsigned char** GetMainLoopOutput(unsigned char** input, vector<vector<unsigned char> > keys)
-	{
-
-		unsigned char*** KeyMatrix;
-		KeyMatrix = new unsigned char**[11];
-		for (int i = 0; i < 11; i++)
-		{
-			KeyMatrix[i] = new unsigned char*[4];
-			for (int j = 0; j < 4; j++)
-			{
-				KeyMatrix[i][j] = new unsigned char[4];
-			}
-		}
-		int KeyMatrixIndex = 0;
-		int d = 0;
-		//cout<<keys.size()<<" "<<keys[0].size();
-		for (int i = 0; i < keys[0].size(); i++)
-		{
-			if ((i) % 4 == 0 && i != 0)
-			{
-				d = 0;
-				KeyMatrixIndex++;
-				//PrintTable(KeyMatrix[KeyMatrixIndex],4);
-			}
-			for (int j = 0; j < keys.size(); j++)
-			{	//cout<<hex(keys[i][j])<<endl;
-				KeyMatrix[KeyMatrixIndex][j][d] = keys[j][i];	// w sumie niepotrzebnie tworze to KeyMatrix
-			}	//ale juz mi sie nie chce przerabiac
-			d++;	// to sa macierze kluczy tworzonych w key schedule
-		}
-
-
-		input = AddRoundKey(input, KeyMatrix[0]);	// tutaj zaczyna sie program wlasciwy, dzialanie zgodne z symulacja
-
-		for (int i = 1; i < 10; i++)
-		{
-			input = GetSubBytes(input);
-			input = GetShiftRowTable(input);
-			input = MixColumn(input);
-			input = AddRoundKey(input, KeyMatrix[i]);
-		}
-
-		input = GetSubBytes(input);
-		input = GetShiftRowTable(input);
-		input = AddRoundKey(input, KeyMatrix[10]);
-
-
-
-		return input;
-	}
-
-
-	void PrintVector(vector<vector<unsigned char> > table)
-	{
-		//cout<<endl;
-		for (int i = 0; i < table.size(); i++)
-		{
-			for (int j = 0; j < table[i].size(); j++)
-			{
-				//cout<< hex(table[i][j])<<" ";
-			}
-			//cout<<endl;
-		}
-	}
-	void PrintTable(unsigned char** table, int size)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = 0; j < size; j++)
-			{
-				cout << hex(table[i][j]) << " ";
-				//std::stringstream ss;
-				//ss<<table[i][j];
-				//cout<<ss.str()<<" ";
-			}
-			cout << endl;
-		}
-	}
-
-};
-
-
-
-
-
-vector<vector<unsigned char> > ConvertArrayToVector(unsigned char Array[][4], vector<vector<unsigned char> > vect)
-{
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			vect[i][j] = Array[i][j];
-		}
-	}
-	return vect;
-}
-
-vector<vector<unsigned char> > GenerateKey(string input)	// tworzenie klucza na podstawie tekstu wejsiowego
-{
-	vector<vector<unsigned char> > out(4, std::vector<unsigned char>(4));
-	int x = 0;
-	int y = 0;
+	std::vector<std::vector<unsigned char>> res(4, std::vector<unsigned char>(4));
+	int col = 0;
+	int row = 0;
 	for (int i = 0; i < input.size(); i++)
 	{
-
-		if ((i) % 1 == 0 && i != 0)
+		if (i != 0)
 		{
-			y++;
+			row++;
+			if (i % 4 == 0)
+			{
+				col++;
+				row = 0;
+			}
 		}
-
-		if (i % 4 == 0 && i != 0)
-		{
-			x++;
-			y = 0;
-		}
-		out[y][x] += input[i];
-		if (x > 3)
+		res[row][col] += input[i];
+		if (col > 3)
 			break;
 	}
-	return out;
+	return res;
 }
 
-unsigned char** GenerateState(string input)	// funkcja uzupelnia macierz stanu przy pomocy wejsiowego tekstu
+std::vector<std::vector<unsigned char>> AES::GenerateEnhancedKey(std::vector<std::vector<unsigned char>> vect1)
+{
+	for (int j = 0; j < 40; j++)
+	{
+		std::vector<unsigned char> v(4);
+		v = AES::MixCols(vect1, j);  // cala operacja rozszerzania wektora
+		for (int i = 0; i < 4; i++)
+		{
+			vect1[i].push_back(v[i]);	//tworzenie kolejnych kluczy, nie robilem oddzielnych tylko wrzucilem do jednego dlugiego wektora
+		}
+	}
+	return vect1;
+}
+
+unsigned char ** AES::GenerateState(std::string input)
 {
 	unsigned char** State = new unsigned char*[4];
 	for (int i = 0; i < 4; i++)
@@ -412,108 +99,261 @@ unsigned char** GenerateState(string input)	// funkcja uzupelnia macierz stanu p
 	return State;
 }
 
-
-
-int main()
+std::vector<unsigned char> AES::MixCols(std::vector<std::vector<unsigned char>> vect1, int IterationNumber)
 {
-	int numOfBlocks;
-	cin >> numOfBlocks;
-
-	double start = omp_get_wtime();
-
-	AES_S aes;
-	for (int i = 0; i < 256; i++)
-	{
-		aes.sbox[i] = s[i];
-	}
-
-
-
-	std::vector<std::vector<unsigned char> > vect1(4, std::vector<unsigned char>(4));
-
-
-	string key = "klucz";
-	//przerobienie klucza na tablice CipherKey
-	vect1 = GenerateKey(key);
-	for (int j = 0; j < 40; j++)
-	{
-		vector<unsigned char> v(4);
-		v = aes.MixColumns(vect1, j);  // cala operacja rozszerzania wektora
+	unsigned char first[4];
+	unsigned char second[4];
+	std::vector<unsigned char> result(4);
+	if (IterationNumber % 4 == 0)
+	{	//PrintVector(vect1);
+		vect1 = AES::RotateAndSubByteKeyColumn(vect1, IterationNumber + 3);
+		//PrintVector(vect1);
 		for (int i = 0; i < 4; i++)
 		{
-			vect1[i].push_back(v[i]);	//tworzenie kolejnych kluczy, nie robilem oddzielnych tylko wrzucilem do jednego dlugiego wektora
+			first[i] = vect1[i][IterationNumber];
+			second[i] = vect1[i][IterationNumber + 3];
+			result[i] = first[i] ^ second[i] ^ AES::Rcon[10 * i + IterationNumber / 4];
+			//cout<<endl<< hex(first[i]) <<"  "<<hex(second[i])<<"  "<<hex(Rcon[10*i + IterationNumber])<<"  "<<hex(result[i])<<endl;
 		}
-
-
 	}
-
-
-	string input = "1234abcd1234abcd";
-	string block = input;
-	for (int i = 1; i < numOfBlocks; i++)
+	else
 	{
-		input += block;
-	}
-
-	double d = input.size() / 16.0;
-	int n = input.size() / 16;
-	if (n < d)
-	{
-		n++;	//n - liczba macierzy stanu
-	}
-
-	//cout<<"LICZBA BLOKOW TEKSTU: "<<n<<endl;
-
-	unsigned char*** AllStates = new unsigned char**[n];	//tworzenie wszystkich macierzy stanu
-	for (int i = 0; i < n; i++)
-	{
-		AllStates[i] = new unsigned char*[4];
-		for (int j = 0; j < 4; j++)
+		for (int i = 0; i < 4; i++)
 		{
-			AllStates[i][j] = new unsigned char[4];
-
+			first[i] = vect1[i][IterationNumber];
+			second[i] = vect1[i][IterationNumber + 3];
+			result[i] = first[i] ^ second[i];
 		}
 	}
 
-	vector<string> Strings(n);		//podzial tekstu do zaszyfrowania na teksty 16 literowe
-	int index = 0;
-	for (int i = 0; i < input.size(); i++)
-	{
-		if (i % 16 == 0 && i != 0)
-		{
-			index++;
-		}
-		Strings[index] += input[i];
-	}
-
-
-	double checkpoint = omp_get_wtime();
-	//cout<<"CZAS PRZYGOTOWANIA: "<<1000*(checkpoint-start)<<endl;
-	cout << n << " " << 1000 * (checkpoint - start) << " ";
-
-
-#pragma omp parallel for
-
-	for (int i = 0; i < n; i++)		//
-	{
-		AllStates[i] = GenerateState(Strings[i]);		// odpowiednie uzupelnianie macierzy stanu
-		//cout<<endl<<Strings[i]<<endl;
-		AllStates[i] = aes.GetMainLoopOutput(AllStates[i], vect1);	// cala operacja szyfrujaca
-
-		//aes.PrintTable(AllStates[i],4);
-	}
-
-	double end = omp_get_wtime();
-	cout << "CZAS LICZENIA: " << 1000 * (end - checkpoint) << endl;
-	cout << 1000 * (end - checkpoint) << endl;
-
-	int f;
-	cin >> f;
-	return 0;
-
-
-
-
+	return result;
 }
 
+std::vector<std::vector<unsigned char>> AES::RotateAndSubByteKeyColumn(std::vector<std::vector<unsigned char> > vect, int Columnindex)
+{
+	unsigned char buffer[4];
+	for (int i = 0; i<vect.size(); i++)
+	{
+		buffer[i] = vect[i][Columnindex];
 
+	}
+
+	unsigned char a = buffer[0];
+	for (int i = 0; i<3; i++)
+	{
+		buffer[i] = buffer[i + 1];
+	}
+	buffer[3] = a;
+	for (int i = 0; i<vect.size(); i++)
+	{
+		vect[i][Columnindex] = buffer[i];
+		vect[i][Columnindex] = AES::getsboxvalue(vect[i][Columnindex]);
+
+	}
+
+	return vect;
+}
+
+unsigned char AES::getsboxvalue(unsigned char x)
+{
+	unsigned char d;
+	for (int i = 0; i<256; i++)
+	{
+		if ((int)x == i)
+		{
+			d = sbox[i];
+
+			break;
+		}
+	}
+	return d;
+}
+
+unsigned char** AES::GetMainLoopOutput(unsigned char** input, std::vector<std::vector<unsigned char> > keys)
+{
+	unsigned char*** KeyMatrix;
+	KeyMatrix = new unsigned char**[11];
+	for (int i = 0; i < 11; i++)
+	{
+		KeyMatrix[i] = new unsigned char*[4];
+		for (int j = 0; j < 4; j++)
+		{
+			KeyMatrix[i][j] = new unsigned char[4];
+		}
+	}
+	int KeyMatrixIndex = 0;
+	int d = 0;
+	//cout<<keys.size()<<" "<<keys[0].size();
+	for (int i = 0; i < keys[0].size(); i++)
+	{
+		if ((i) % 4 == 0 && i != 0)
+		{
+			d = 0;
+			KeyMatrixIndex++;
+			//PrintTable(KeyMatrix[KeyMatrixIndex],4);
+		}
+		for (int j = 0; j < keys.size(); j++)
+		{	//cout<<hex(keys[i][j])<<endl;
+			KeyMatrix[KeyMatrixIndex][j][d] = keys[j][i];	// w sumie niepotrzebnie tworze to KeyMatrix
+		}	//ale juz mi sie nie chce przerabiac
+		d++;	// to sa macierze kluczy tworzonych w key schedule
+	}
+
+
+	input = AddRoundKey(input, KeyMatrix[0]);	// tutaj zaczyna sie program wlasciwy, dzialanie zgodne z symulacja
+
+	for (int i = 1; i < 10; i++)
+	{
+		input = GetSubBytes(input);
+		input = GetShiftRowTable(input);
+		input = MixColumn(input);
+		input = AddRoundKey(input, KeyMatrix[i]);
+	}
+
+	input = GetSubBytes(input);
+	input = GetShiftRowTable(input);
+	input = AddRoundKey(input, KeyMatrix[10]);
+
+
+
+	return input;
+}
+
+unsigned char** AES::AddRoundKey(unsigned char** matrix, unsigned char** key, int size)
+{
+	unsigned char**result = new unsigned char*[size];
+	for (int i = 0; i < size; i++)
+	{
+		result[i] = new unsigned char[size];
+		for (int j = 0; j < size; j++)
+		{
+			result[i][j] = matrix[i][j] ^ key[i][j];
+		}
+	}
+	return result;
+}
+
+unsigned char ** AES::GetSubBytes(unsigned char ** tablica)
+{
+	unsigned char **newchar = new unsigned char *[4];
+	for (int i = 0; i < 4; i++)
+	{
+		newchar[i] = new unsigned char[4];
+		for (int j = 0; j < 4; j++)
+		{
+			newchar[i][j] = getsboxvalue(tablica[i][j]);
+
+			//cout << hex(newchar[i][j]) << "  ";
+		}
+		//cout << endl;
+	}
+	return newchar;
+}
+
+unsigned char ** AES::GetShiftRowTable(unsigned char ** firsttable)
+{
+	unsigned char** mat = new unsigned char*[4];
+	int j = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		mat[i] = new unsigned char[4];
+		unsigned char* dd = new unsigned char[4];
+		for (int d = 0; d < j; d++)
+		{
+			dd = RotateLeft(firsttable[i]);
+		}
+		j++;
+	}
+	return firsttable;
+}
+
+unsigned char * AES::RotateLeft(unsigned char * row)
+{
+	unsigned char* buffer = new unsigned char;
+	*buffer = row[0];
+	for (int i = 0; i < 3; i++)
+	{
+		row[i] = row[i + 1];
+	}
+	row[3] = *buffer;
+	return row;
+}
+
+std::vector<unsigned char> AES::MixColumns(std::vector<std::vector<unsigned char> > vect1, int IterationNumber)
+{
+	unsigned char first[4];
+	unsigned char second[4];
+	std::vector<unsigned char> result(4);
+	if (IterationNumber % 4 == 0)
+	{	//PrintVector(vect1);
+		vect1 = RotateAndSubByteKeyColumn(vect1, IterationNumber + 3);
+		//PrintVector(vect1);
+		for (int i = 0; i < 4; i++)
+		{
+			first[i] = vect1[i][IterationNumber];
+			second[i] = vect1[i][IterationNumber + 3];
+			result[i] = first[i] ^ second[i] ^ Rcon[10 * i + IterationNumber / 4];
+			//cout<<endl<< hex(first[i]) <<"  "<<hex(second[i])<<"  "<<hex(Rcon[10*i + IterationNumber])<<"  "<<hex(result[i])<<endl;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			first[i] = vect1[i][IterationNumber];
+			second[i] = vect1[i][IterationNumber + 3];
+			result[i] = first[i] ^ second[i];
+		}
+	}
+
+	return result;
+}
+
+unsigned char ** AES::MixColumn(unsigned char ** matrix)
+{
+	unsigned char **mat = new unsigned char*[4];
+	unsigned char column[4];
+	for (int i = 0; i < 4; i++)
+	{
+		mat[i] = new unsigned char[4];
+
+	}
+	for (int j = 0; j < 4; j++)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			column[i] = matrix[i][j];
+		}
+		gmix_column(column);
+		for (int i = 0; i < 4; i++)
+		{
+			mat[i][j] = column[i];
+		}
+	}
+	return mat;
+}
+
+void AES::gmix_column(unsigned char r[]) {		// ta funkcja jest skopiowana z neta, srednio ja ogarniam
+	unsigned char a[4];	// operacja mixcolumns
+	unsigned char b[4];
+	unsigned char c;
+	unsigned char h;
+	/* The array 'a' is simply a copy of the input array 'r'
+	* The array 'b' is each element of the array 'a' multiplied by 2
+	* in Rijndael's Galois field
+	* a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field */
+	for (c = 0; c < 4; c++) {
+		a[c] = r[c];
+		/* h is 0xff if the high bit of r[c] is set, 0 otherwise */
+		h = (unsigned char)((signed char)r[c] >> 7);
+		/* arithmetic right shift, thus shifting in either zeros or ones */
+		b[c] = r[c] << 1;
+		/* implicitly removes high bit because b[c] is an 8-bit char,
+		so we xor by 0x1b and not 0x11b in the next line */
+		b[c] ^= 0x1B & h; /* Rijndael's Galois field */
+	}
+	r[0] = (b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]); /* 2 * a0 + a3 + a2 + 3 * a1 */
+	r[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; /* 2 * a1 + a0 + a3 + 3 * a2 */
+	r[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; /* 2 * a2 + a1 + a0 + 3 * a3 */
+	r[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
+}
--- PorrAes.cpp
+++ PorrAes.cpp
@@ -8,7 +8,6 @@
 # include <string>
 #include <sstream>
 #include <vector>
-#include <mpi.h>
 #include <ctime>
 #include "Helper.h"
 
@@ -51,8 +50,6 @@ int main()
 		n++;	//n - liczba macierzy stanu
 	}
 
-	//cout<<"LICZBA BLOKOW TEKSTU: "<<n<<endl;
-	//in class
 	unsigned char*** AllStates = new unsigned char**[n];	//tworzenie wszystkich macierzy stanu
 	for (int i = 0; i < n; i++)
 	{
@@ -64,8 +61,7 @@ int main()
 		}
 	}
 
-	//in class
-	vector<string> Strings(n);		//podzial tekstu do zaszyfrowania na teksty 16 literowe
+	vector<string> Strings(n);
 	int index = 0;
 	for (int i = 0; i < input.size(); i++)
 	{
@@ -78,19 +74,14 @@ int main()
 
 
 	double checkpoint = omp_get_wtime();
-	//cout<<"CZAS PRZYGOTOWANIA: "<<1000*(checkpoint-start)<<endl;
 	cout << n << " " << 1000 * (checkpoint - start) << " ";
 
-
+	omp_set_num_threads(2);
 #pragma omp parallel for
-	cout << "ZASZYFROWANE";
-	for (int i = 0; i < n; i++)		//
+	for (int i = 0; i < n; i++)
 	{
-		AllStates[i] = aes.GenerateState(Strings[i]);		// odpowiednie uzupelnianie macierzy stanu
-		//cout<<endl<<Strings[i]<<endl;
-		AllStates[i] = aes.GetMainLoopOutput(AllStates[i], vect1);	// cala operacja szyfrujaca
-		Helper::PrintTable(AllStates[i], 4);
-		//aes.PrintTable(AllStates[i],4);
+		AllStates[i] = aes.GenerateState(Strings[i]);
+		AllStates[i] = aes.GetMainLoopOutput(AllStates[i], vect1);
 	}
 
 	double end = omp_get_wtime();
@@ -100,140 +91,4 @@ int main()
 	int f;
 	cin >> f;
 	return 0;
-
-	//int numOfBlocks = 10000;
-	////cout<<"Podaj liczbê bloków do testowego szyfrowania:\n";
-	////cin>>numOfBlocks;
-
-	//clock_t begin = clock();
-
-
-	//AES aes;
-	//for (int i = 0; i < 256; i++)
-	//{
-	//	aes.sbox[i] = s[i];
-	//}
-
-
-
-	//std::vector<std::vector<unsigned char> > vect1(4, std::vector<unsigned char>(4));
-
-
-	//string key = "klucz";
-	////przerobienie klucza na tablice CipherKey
-	//vect1 = GenerateKey(key);
-	//for (int j = 0; j<40; j++)
-	//{
-	//	vector<unsigned char> v(4);
-	//	v = aes.MixColumns(vect1, j);  // cala operacja rozszerzania wektora
-	//	for (int i = 0; i<4; i++)
-	//	{
-	//		vect1[i].push_back(v[i]);	//tworzenie kolejnych kluczy, nie robilem oddzielnych tylko wrzucilem do jednego dlugiego wektora
-	//	}
-
-
-	//}
-
-
-	//string input = "1234abcd1234abcd";
-	//string block = input;
-	//for (int i = 1; i<numOfBlocks; i++)
-	//{
-	//	input += block;
-	//}
-
-	//double d = input.size() / 16.0;
-	//int n = input.size() / 16;
-	//if (n<d)
-	//{
-	//	n++;	//n - liczba macierzy stanu
-	//}
-
-	////cout<<"LICZBA BLOKOW TEKSTU: "<<n<<endl;
-
-
-	//vector<string> Strings(n);		//podzial tekstu do zaszyfrowania na teksty 16 literowe
-	//int index = 0;
-	//for (int i = 0; i<input.size(); i++)
-	//{
-	//	if (i % 16 == 0 && i != 0)
-	//	{
-	//		index++;
-	//	}
-	//	Strings[index] += input[i];
-	//}
-
-	//unsigned char*** AllStates = new unsigned char**[n];	//tworzenie wszystkich macierzy stanu
-	//for (int i = 0; i<n; i++)
-	//{
-	//	AllStates[i] = new unsigned char*[4];
-	//	for (int j = 0; j<4; j++)
-	//	{
-	//		AllStates[i][j] = new unsigned char[4];
-
-	//	}
-	//}
-
-
-
-
-
-	//// Initialize the MPI environment
-	//MPI_Init(NULL, NULL);
-
-	//// Get the number of processes
-	//int world_size;
-	//MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	//// Get the rank of the process
-	//int world_rank;
-	//MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-
-
-
-
-
-	//clock_t checkpoint = clock();
-
-	//for (int i = 1; i<world_size; i++)
-	//{
-	//	if (world_rank == i) {
-	//		for (int j = i - 1; j<n; j += world_size - 1)
-	//		{
-	//			AllStates[j] = GenerateState(Strings[j]);
-	//			AllStates[j] = aes.GetMainLoopOutput(AllStates[j], vect1);
-	//			MPI_Send(AllStates[j], 4, MPI_BYTE, 0, j, MPI_COMM_WORLD);
-	//		}
-	//	}
-	//}
-	//if (!world_rank)
-	//{
-	//	for (int i = 0; i<n; i++)
-	//	{
-	//		MPI_Recv(AllStates[i], 4, MPI_BYTE, (i % (world_size - 1)) + 1, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	//		//cout<<"WATEK "<<world_rank<<" !!!!!!\n";
-	//	}
-	//	for (int i = 0; i<n; i++)
-	//	{
-	//		//cout<<AllStates[i][1][1];
-	//	}
-	//}
-
-
-
-
-	//MPI_Finalize();
-
-	//clock_t end = clock();
-	//if (!world_rank) {
-	//	cout << world_rank << ": Czas przygotowania: " << double(checkpoint - begin) / CLOCKS_PER_SEC << endl;
-	//	cout << world_rank << ": Czas równoleg³ego szyfrowania: " << double(end - checkpoint) / CLOCKS_PER_SEC << endl;
-	//	//cout<<CLOCKS_PER_SEC;
-	//	cout << "koniec";
-	//}
-	//return 0;
-
-
 }
-
