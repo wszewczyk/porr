5d5e8596bce2057118568cf3fb622cde063aa7cc
Wlodzimierz Szewczyk <wlodzimierz.szewczyk@qualent.eu>
2017-01-22 21:50:37 +0100

    Refactoring.

--- Porr MPI/PorrAes.cpp
+++ Porr MPI/PorrAes.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <ctime>
 #include <mpi.h>
+#include <fstream>
 #include "Helper.h"
 
 using namespace std;
@@ -16,23 +17,23 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	clock_t begin = clock();
-	int numOfBlocks = 10000;
+	int numOfBlocks = 100000;
 
 	double start = omp_get_wtime();
 
-	std::vector<std::vector<unsigned char>> vect1(4, std::vector<unsigned char>(4));
+	std::vector<std::vector<unsigned char>> enchancedKey(4, std::vector<unsigned char>(4));
 
 	string key = "abcdefgh";
 	//Generating array for key.
 	AES aes = AES();
-	vect1 = aes.GenerateKey(key);
+	enchancedKey = aes.GenerateKey(key);
 	for (int j = 0; j < 40; j++)
 	{
 		vector<unsigned char> v(4);
-		v = aes.MixColumns(vect1, j);  // extanding vector
+		v = aes.MixColumns(enchancedKey, j);  // extanding vector
 		for (int i = 0; i < 4; i++)
 		{
-			vect1[i].push_back(v[i]);	//Generating keys in one big vector (instead of separated)
+			enchancedKey[i].push_back(v[i]);	//Generating keys in one big vector (instead of separated)
 		}
 	}
 
@@ -98,7 +99,7 @@ int main(int argc, char* argv[])
 			for (int j = i - 1; j < n; j += world_size - 1)
 			{
 				AllStates[j] = aes.GenerateState(Strings[j]);
-				AllStates[j] = aes.Encrypt(AllStates[j], vect1);
+				AllStates[j] = aes.Encrypt(AllStates[j], enchancedKey);
 				MPI_Send(AllStates[j], 4, MPI_BYTE, 0, j, MPI_COMM_WORLD);
 			}
 		}
@@ -115,11 +116,18 @@ int main(int argc, char* argv[])
 
 	clock_t end = clock();
 	if (!world_rank) {
-		cout << world_rank << ": Czas przygotowania: " << double(checkpoint - begin) / CLOCKS_PER_SEC << endl;
-		cout << world_rank << ": Czas równoleg³ego szyfrowania: " << double(end - checkpoint) / CLOCKS_PER_SEC << endl;
+		cout << world_rank << ": Czas przygotowania: " << double(checkpoint - begin) << endl;
+		cout << world_rank << ": Czas równoleg³ego szyfrowania: " << double(end - checkpoint) << endl;
+		ofstream myfile;
+		myfile.open("results.txt", std::ios::app);
+		myfile << numOfBlocks << " " << double(end - checkpoint)  << "\n";
+		myfile.close();
 	}
+
+
+
+	int finish;
+	std::cin >> finish;
+
 	return 0;
-
-
-
 }
